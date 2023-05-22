c40bf186b4a72b31e929deeae6fcbe5babcd0913
Harmones81 <52055382+Harmones81@users.noreply.github.com>
2023-05-22 11:14:08 -0400

    added db communication functionalities

--- src/Model/ItemDB.java
+++ src/Model/ItemDB.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ItemDB
+public class ItemDB extends DB
 {
     private static ItemDB instance;
 
